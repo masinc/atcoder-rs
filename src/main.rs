e34c79aad70ec59f97a74feb0bc83bb4cd553871
masinc <masinc000@gmail.com>
2021-10-09 04:05:30 +0900

    answer

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
+use itertools::Itertools;
 use proconio::input;
 use std::io::{prelude::*, BufWriter};
 
 type TResult<T> = Result<T, Box<dyn std::error::Error>>;
+
+fn process(source: &str, words: &[&str]) -> bool {
+    if source.is_empty() {
+        return true;
+    }
+
+    let target_words = words
+        .iter()
+        .filter(|&&w| source.starts_with(w))
+        .cloned()
+        .collect_vec();
+
+    for word in target_words {
+        if process(&source[word.len()..], words) {
+            return true;
+        }
+    }
+
+    false
+}
 
 fn run(mut stdout: impl Write) -> TResult<()> {
     // Write here
     // writeln!(stdout, "{}", s)?;
 
     input! {
-        a: u64,
-        b: u64, c: u64,
         s: String,
     }
 
-    writeln!(stdout, "{} {}", a + b + c, s)?;
+    let mut words = "dream dreamer erase eraser"
+        .split_ascii_whitespace()
+        .collect_vec();
+    words.sort_by_key(|w| w.len());
+    words.reverse();
+    let words = words;
+
+    let result = process(&s, &words);
+    writeln!(stdout, "{}", if result { "YES" } else { "NO" })?;
 
     Ok(())
 }
